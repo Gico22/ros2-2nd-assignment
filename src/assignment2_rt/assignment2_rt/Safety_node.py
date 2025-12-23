4e48beabb74918a0eb1bcd93b0b24dd4a628ca09
Gico22 <giacomonogarin@gmail.com>
2025-12-23 17:06:42 +0100

    Added services and safety controller

--- src/assignment2_rt/assignment2_rt/Safety_node.py
+++ src/assignment2_rt/assignment2_rt/Safety_node.py
@@ -13,26 +13,69 @@ class Safety(Node):
         super().__init__('Safety')
 
         # publishers
-        self.obstacle_pub = self.create_publisher(ObstacleInfo, '/Obstacle_info', 10)
-        self.safety_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.obstacle_pub = self.create_publisher(ObstacleInfo, '/obstacle_info', 10)
+        self.vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
 
         # subscribers
-        self.vel_sub = self.create_subscriber(Twist, '/cmd_vel', self.vel_read_callback, 10)
-        self.scan_sub = self.create_subscriber(LaserScan, '/scan', self.scan_read_callback, 10)
+        self.vel_sub = self.create_subscription(Twist, '/cmd_vel_user', self.vel_read_callback, 10)
+        self.scan_sub = self.create_subscription(LaserScan, '/scan', self.scan_read_callback, 10)
+
+        # services
+        self.set_threshold_srv = self.create_service(SetThreshold, '/set_threshold', self.set_threshold_callback)
+        self.get_averages_srv = self.create_service(GetAverages, '/get_averages', self.get_averages_callback)
 
         # variables
-        self.safety_twist = Twist
+        self.user_twist = Twist()
+        self.safety_twist = Twist()
+        self.stop = Twist()
+        self.scan_received = False
+        self.safe = True
+        self.vel_history= []
 
         self.obstacle_info = ObstacleInfo()
         self.obstacle_info.distance = self.obstacle_info.direction = None
-        self.threshold = self.obstacle_info.threshold = 1
+        self.threshold = self.obstacle_info.threshold = 1   # default threshold
 
+        # create timer
+        self.timer = self.create_timer(0.1, self.safety_controller) 
+
+    def safety_controller(self):
+        if not self.scan_received:
+            return
+        
+        # publish the obstacle informations
+        self.obstacle_pub.publish(self.obstacle_info)
+
+        # publishes the user velocities if the robot hasn't passed the threshold
+        if self.safe:
+            self.vel_pub.publish(self.user_twist)
+
+        # publish inverse velocities if threshold is passed 
+        if self.obstacle_info.distance <= self.threshold:
+            self.safe = False
+            self.vel_pub.publish(self.safety_twist)
+        
+        # stop the robot after it reaches the safe zone
+        if not self.safe and self.obstacle_info.distance > (self.threshold + 0.1):
+            self.safe = True
+            self.vel_pub.publish(self.stop)
 
     def vel_read_callback(self, vel):
+        # publishes the user velocities
+        self.user_twist.linear.x = vel.linear.x
+        self.user_twist.angular.z = vel.angular.z
+
+        # updates safety twist
         self.safety_twist.linear.x = -vel.linear.x
         self.safety_twist.angular.z = -vel.angular.z
 
+        # memorize the last 5 user inputs
+        self.vel_history.append((vel.linear.x, vel.angular.z))
+        if len(self.vel_history) > 5:
+            self.vel_history.pop(0)
+
     def scan_read_callback(self, scanner):
+        self.scan_received = True
         # compute valid minimum obstacle distance
         valid_measurements = [
             (i, r) for i, r in enumerate(scanner.ranges)
@@ -51,6 +94,25 @@ class Safety(Node):
             self.obstacle_info.direction = "Right"
         else:
             self.obstacle_info.direction = "Front"  
+
+    def set_threshold_callback(self, request, response):
+        if request.threshold <= 0:
+            self.threshold = self.obstacle_info.threshold = 0.1
+            response.success = False
+            response.message = f"Input threshold too low, threshold set to 0.1"
+        else:
+            self.threshold = self.obstacle_info.threshold = request.threshold
+            response.success = True
+            response.message = f"Threshold set to {self.threshold}"
+        return response
+    
+    def get_averages_callback(self, request, response):
+        if len(self.vel_history) == 0:
+            response.linear_avg = response.angular_avg = 0
+            return response
+        response.linear_avg = sum(v for v, _ in self.vel_history) / len(self.vel_history)
+        response.angular_avg = sum(w for _, w in self.vel_history) / len(self.vel_history)
+        return response
 
 def main(args=None):
     rclpy.init(args = args)
--- src/assignment2_rt/assignment2_rt/UI_node.py
+++ src/assignment2_rt/assignment2_rt/UI_node.py
@@ -1,9 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
 from assignment2_rt_interfaces.srv import SetThreshold, GetAverages
-import time
 
 class UI(Node):
 
@@ -11,11 +9,19 @@ class UI(Node):
         super().__init__('UI')
 
         # publishers
-        self.vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.nodes_vel_pub = self.create_publisher(Twist, '/cmd_vel_user', 10)
+
+        # services
+        self.set_threshold_client = self.create_client(SetThreshold, '/set_threshold')
+        self.get_averages_client = self.create_client(GetAverages, '/get_averages')
+        while not self.set_threshold_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Waiting for set_threshold service...')
+        while not self.get_averages_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Waiting for get_averages service...')
 
         # variables
         self.twist = Twist()        # robot velocity (linear and angular)
-        self.stop = Twist()         # robot stop command (twist with '0' values)
+        self.linear_average = self.angular_average = 0  # average velocities values
 
         # create timer
         self.timer = self.create_timer(0.1, self.user_interface) 
@@ -32,12 +38,40 @@ class UI(Node):
         self.twist.linear.x = self.clamp(x, -0.5, 0.5)
         self.twist.angular.z = self.clamp(z, -1.0, 1.0)
 
-        # publish the velocity for 1 second
-        self.vel_pub.publish(self.twist)
-        time.sleep(5)
-        self.vel_pub.publish(self.stop)
+        # sends the velocity to safety node, which will publish it to the robot
+        self.nodes_vel_pub.publish(self.twist)
         self.twist = Twist()
 
+        while True:
+            user_decision = input("Select one of the following options:" \
+            "'1': select robot velocities" \
+            "'2': set a new threshold value for the robot" \
+            "'3': get the average values of your last 5 inputs")
+            if user_decision in {"1", "2", "3"}:
+                break
+            print("Invalid input")
+
+        match user_decision:
+            case "1":
+                return
+            case "2":
+                req = SetThreshold.Request()
+                req.threshold = float(input("Select the desired threshold (must be > 0)"))
+                if req.threshold <= 0:
+                    req.threshold = 0
+                future = self.set_threshold_client.call_async(req)
+                rclpy.spin_until_future_complete(self, future)
+                response = future.result()
+                print(response.success, response.message)
+            case "3":
+                req = GetAverages.Request()
+                future = self.get_averages_client.call_async(req)
+                rclpy.spin_until_future_complete(self, future)
+                response = future.result()
+                self.linear_average = response.linear_avg
+                self.angular_average = response.angular_avg
+
+            
     def clamp(self, val, vmin, vmax):
         return max(vmin, min(val, vmax))
 
